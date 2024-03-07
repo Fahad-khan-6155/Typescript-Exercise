db1ffef2f6dff0d38e7a7947fe189a258b045824
Fahad-khan-6155 <162614832+Fahad-khan-6155@users.noreply.github.com>
2024-03-07 22:14:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,2 @@
+var personName = "ERIT";
+Console.LOG("hello ".concat(personName, " would you like to learn some python today?"));
---
+++ app.ts
@@ -0,0 +1,2 @@
+let personName: string = "ERIT";
+Console.LOG(`hello ${personName} would you like to learn some python today?`);
